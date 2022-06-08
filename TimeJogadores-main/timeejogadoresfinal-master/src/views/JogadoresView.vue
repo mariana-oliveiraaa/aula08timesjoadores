8a62e657489f5592599a1f7385c43ecaeb6f2e11
mariana-oliveiraaa <99214209+mariana-oliveiraaa@users.noreply.github.com>
2022-06-08 09:26:13 -0300

    Add files via upload

---
+++ TimeJogadores-main/timeejogadoresfinal-master/src/views/JogadoresView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container"> 
+    <div class="title"> 
+      <h2>Gerenciamento de jogadores </h2>
+    </div>
+    <div class="form-input">
+      <input type="text" placeholder="Nome">
+      <input type="text" placeholder="Time">
+      <button>Salvar</button>
+    </div>
+    <div class="list-items">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Times</th>
+            <th>Ações</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Jogador1</td>
+            <td>???</td>
+            <td>time1</td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jogador2</td>
+            <td>???</td>
+            <td>time2</td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Jogador3</td>
+            <td>???</td>
+            <td>time3</td>
+            <td>????</td>
+          </tr>
+        
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
---
+++ TimeJogadores-main/timeejogadoresfinal-master/src/views/TimesView.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+ data(){
+   return {
+     times: [
+       {id: 1, name:"Time 1"},
+       {id: 2, name:"Time 2"},
+       {id: 3, name:"Time 3"},
+       {id: 4, name:"Time 4"},
+     ],
+    };
+   },
+ };
+</script>
+
+
+<template>
+  <div class="container"> 
+    <div class="title"> 
+      <h2>Gerenciamento de Times </h2>
+    </div>
+    <div class="form-input">
+      <input type="text" />
+      <button>Salvar</button>
+   </div>
+   <div class="list-items">
+    <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Times</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="time in times " :key="time.id">
+          <td>{{ time.id }}</td>
+          <td>{{ time.name }}</td>
+          <td>???</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+  </div>
+</template>
+
+
